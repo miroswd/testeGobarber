26307c4f94bbc0c44ea10e5b167d0dab7b652cbf
miroswald <altamirsantos93@hotmail.com>
2019-10-29 20:37:04 -0300

    Validação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,7 +16,6 @@ class UserController {
     // Verifica se o req.body recebe os valores validados
     if (!(await schema.isValid(req.body))) {
       // Se falhar retorna false
-
       return res.status(400).json({ errror: 'Validation fails' });
     } // Se for válido retorna true
 
@@ -34,27 +33,23 @@ class UserController {
 
   async update(req, res) {
     const schema = Yup.object().shape({
-      // não obrigatório, pois é update
       name: Yup.string(),
       email: Yup.string().email(),
-      oldPassword: Yup.string()
-        .required()
-        .min(6), // Se a senha antiga for informada, a nova tmb precisa ser informada
+      oldPassword: Yup.string().min(6),
       password: Yup.string()
         .min(6)
         .when('oldPassword', (oldPassword, field) =>
           oldPassword ? field.required() : field
-        ), // Quando a oldPassword for preenchida, a password deve ser obrigatória
+        ),
       confirmPassword: Yup.string().when('password', (password, field) =>
         password ? field.required().oneOf([Yup.ref('password')]) : field
-      ), // Se o password estiver preenchido, esse campo precisa ser igual
+      ),
     });
 
     // Verifica se o req.body recebe os valores validados
     if (!(await schema.isValid(req.body))) {
       // Se falhar retorna false
-
-      return res.status(400).json({ errror: 'Validation fails' });
+      return res.status(400).json({ error: 'Validation fails' });
     } // Se for válido retorna true
 
     // console.log(req.userId); // Agora consigo usar o userId para fazer as alterações
